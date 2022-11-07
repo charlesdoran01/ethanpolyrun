9c9a2e4fff133d65f673c55a06d57da961b2c1b4
charlesdoran01 <113252036+charlesdoran01@users.noreply.github.com>
2022-11-07 17:30:55 -0500

    initial

---
+++ EB - PolyRun/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Jumping power for the player object
+    [Header("Default Jumping Power")]
+    public float jumpPower = 6f;
+    //True or false if the objectis on the ground
+    [Header ("Boolean isGrounded")]
+    public bool isGrounded = false;
+    //Position of the object
+    float posX = 0.0f;
+    //Rigidbody2D of the object
+    Rigidbody2D rb;
+
+// Start is called before the first frame update
+    void Start()
+    {
+        //Variable rb equals to Rigidboy2D
+        //component
+        rb = transform.GetComponent<Rigidbody2D>();
+        //Variable posX equals to Position
+        //of the object on the x axis
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //If the spacebar is pressed and
+        //object is on the ground and
+        //the game is playing
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            //Adds force to the object
+            //to jump upwards based on
+            //jump power, mass, and
+            //gravity
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+        }
+
+        //If the player position is less than
+        //the original position of the player
+        if (transform.position.x < posX)
+        {
+            //Execute GameOver function
+            GameOver();
+        }
+    }
+
+    //when an incoming collider makes contact
+    //with this object's collider
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //If colliders tag equals ground
+        if (collision.collider.tag == "Ground")
+        {
+            //isGrounded equals True
+            isGrounded = true;
+        }
+
+        //If collider tag equals enemy
+        if (collision.collider.tag == "Enemy")
+        {
+            //Game Over function is called
+            GameOver();
+        }
+    }
+
+    //when a collider on another object is touching
+    //this object's collider
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        //If colliders tag equals Ground
+        if (collision.collider.tag == "Ground")
+        {
+            //isGrounded equals true
+            isGrounded = true;
+        }
+    }
+
+    //when a collider on another object is touching
+    //this object's collider
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        //If colliders tag equals ground
+        if (collision.collider.tag == "Ground")
+        {
+            //isGrounded equals true
+            isGrounded = false;
+        }
+    }
+
+    //Game over function
+    void GameOver()
+    {
+        //Game Over function is called from the game manager
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
+
+    //when a collider on another object is touching
+    //this object's trigger
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If triggers tag equals coin
+        if (collision.tag == "Coin")
+        {
+            //Call IncrementScore from
+            //Game Controller
+            GameObject.Find("GameController").GetComponent<GameController>().IncrementScore();
+            //Destroy object
+            Destroy(collision.gameObject);
+        }
+    }
+}
